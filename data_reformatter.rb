4b4653e024faa5f50a95f41b5c5e4582f36946c1
Millertron <senjinkoha@gmail.com>
2019-04-25 15:17:52 +0100

    Rubocop fix
    Extract method

--- data_reformatter.rb
+++ data_reformatter.rb
@@ -1,31 +1,33 @@
-def read_and_format(line, output, entry_date)
-  return nil if line.strip.empty?
-  
-  if ( (/\d{4}\/\d{2}\/\d{2}/).match(line[0...10]) )
-    return line[0...10]
-  elsif entry_date 
-    if (line.split("\t").size == 3)
-      output.write("\r\n#{entry_date} ")
-      output.write(line.gsub("\n", '').gsub("\r", '').gsub("\t", '#!%'))
-    else
-      output.write(" #{line}".gsub("\n", '').gsub("\r", ''))
-    end
-  end
-  return entry_date
+# frozen_string_literal: true
 
+def write_to_output(line, output, entry_date)
+  if line.split("\t").size == 3
+    output.write("\r\n#{entry_date} ")
+    output.write(line.delete("\n").delete("\r").gsub("\t", '#!%'))
+  else
+    output.write(" #{line}".delete("\n").delete("\r"))
+  end
 end
 
-def reformat input_path
+def read_and_format(line, entry_date, output)
+  return nil if line.strip.empty?
+  return line[0...10] if %r{\d{4}/\d{2}/\d{2}}.match?(line[0...10])
+
+  write_to_output(line, output, entry_date) if entry_date
+
+  entry_date
+end
+
+def reformat(input_path)
   output = File.open('output/data.txt', 'w')
 
   File.open(input_path, 'r') do |file|
-
     entry_date = nil
-    lines = file.each_line do |line|
-      entry_date = read_and_format(line, output, entry_date)
+    file.each_line do |line|
+      entry_date = read_and_format(line, entry_date, output)
     end
   end
-  
+
   output.close
 end
 
